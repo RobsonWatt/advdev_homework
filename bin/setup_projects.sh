7201ca078645f60374974cb360972a87c3a5b756
robson <nexus@nexus.com>
2019-10-05 20:09:53 -0300

    setup_projects.sh

--- bin/setup_projects.sh
+++ bin/setup_projects.sh
@@ -12,7 +12,7 @@ GUID=$1
 USER=$2
 FROM_JENKINS=$3
 
-oc login https://master.na311.openshift.opentlc.com:443 --token=7YBExVCDDIbi2Y70Zx_Qo7_Gk-fqdKBABMD7YnKCSGY --insecure-skip-tls-verify
+oc login https://master.na311.openshift.opentlc.com:443 --token=fjtan4wiNz1MoM2hhrEzkoFwTSB2F_TRDFVnXF6hqTE --insecure-skip-tls-verify
 
 echo "Creating Homework Projects for GUID=${GUID} and USER=${USER}"
 oc new-project ${GUID}-jenkins    --display-name="${GUID} AdvDev Homework Jenkins"
